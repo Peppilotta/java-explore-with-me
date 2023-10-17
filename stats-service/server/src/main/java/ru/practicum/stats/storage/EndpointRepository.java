6092c70b9d62f70c53c2dbef75906fe56e694ea1
oandreeva <oandreeva@micromash.ru>
2023-10-17 12:15:45 +0300

    fix: add order by

--- stats-service/server/src/main/java/ru/practicum/stats/storage/EndpointRepository.java
+++ stats-service/server/src/main/java/ru/practicum/stats/storage/EndpointRepository.java
@@ -29,7 +29,8 @@ public interface EndpointRepository extends JpaRepository<Endpoint, Long>, JpaSp
             "inner join e.visitors v " +
             "where e.uri in :uris " +
             "and v.timestamp between :start and :end " +
-            "group by e.app, e.uri")
+            "group by e.app, e.uri " +
+            "order by count(distinct v.ip) desc")
     List<VisitorsStatsDto> getUniqueUrisFromList(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("select new ru.practicum.stats.VisitorsStatsDto(e.app, e.uri, count(v.ip)) " +
@@ -37,20 +38,23 @@ public interface EndpointRepository extends JpaRepository<Endpoint, Long>, JpaSp
             "inner join e.visitors v " +
             "where e.uri in :uris " +
             "and v.timestamp between :start and :end " +
-            "group by e.app, e.uri")
+            "group by e.app, e.uri " +
+            "order by count(v.ip) desc")
     List<VisitorsStatsDto> getUrisFromList(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("select new ru.practicum.stats.VisitorsStatsDto(e.app, e.uri, count(v.ip)) " +
             "from Endpoint as e " +
             "inner join e.visitors v " +
             "where v.timestamp between :start and :end " +
-            "group by e.app, e.uri")
+            "group by e.app, e.uri " +
+            "order by count(v.ip) desc")
     List<VisitorsStatsDto> getAllUrisFromList(LocalDateTime start, LocalDateTime end);
 
-    @Query("select new ru.practicum.stats.VisitorsStatsDto(e.app, e.uri, count(distinct v.ip)) " +
+    @Query("select new ru.practicum.stats.VisitorsStatsDto(e.app, e.uri, count(distinct v.ip))  " +
             "from Endpoint as e " +
             "inner join e.visitors v " +
             "where v.timestamp between :start and :end " +
-            "group by e.app, e.uri")
+            "group by e.app, e.uri " +
+            "order by count(distinct v.ip) desc ")
     List<VisitorsStatsDto> getAllUniqueUrisFromList(LocalDateTime start, LocalDateTime end);
 }
