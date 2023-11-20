24b560142ceb2d1a85d0fd4889b78dff1ca8e4aa
oandreeva <oandreeva@micromash.ru>
2023-11-20 23:56:15 +0300

    fix: compilation edited back

--- ewm-service/src/main/java/ru/practicum/compilation/dto/UpdateCompilationRequest.java
+++ ewm-service/src/main/java/ru/practicum/compilation/dto/UpdateCompilationRequest.java
@@ -26,6 +26,6 @@ public class UpdateCompilationRequest {
 
     private Boolean pinned;
 
-    @Size(min = 1, max = 51, message = "Size of title must be between {min} and {max}")
+    @Size(min = 1, max = 50, message = "Size of title must be between {min} and {max}")
     private String title;
 }
